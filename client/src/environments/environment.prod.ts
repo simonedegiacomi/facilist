9d4f4a6cb1ad6bfbef56f64970efeb55bd8d8a2f
simonedegiacomi <simonedegiacomi97@gmail.com>
2018-12-24 18:22:00 +0100

    Add public key for push notification in production environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+    production: true,
+    publicVAPIDKey: "BArkB_1gSLGOPruj4ZC3HeRmq9ncz4vKrDomlNXpOYEeJircD7VPXCY-3Px4MjzhvocOWFmW2c9zy9HHKKbTE4Q"
 };
